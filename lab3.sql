c37ac4b388215607b59ea43cefa472bc8a342d5e
Lauren Ayres <143033877+lkayres@users.noreply.github.com>
2023-09-13 16:00:45 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -3,7 +3,7 @@ insert into student (s_email,s_name,major)
 values ('s1@jmu.edu', 's1', 'IA'), 
   ('s2@jmu.edu', 's2', 'IA'),
   ('s3@jmu.edu', 's3', 'ISAT'),
-  ('s4@jmu.edu', 's4', 'ISAT');
+  ('s4@jmu.edu', 's4', 'ISAT')
 
 --Q2.2
 insert into professor (p_email,p_name,office)
